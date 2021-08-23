d87760e61e2a27c8f6370583abaf313e1fa52493
Oscarwu <wuyuta@deakin.edu.au>
2021-08-23 17:01:56 +1000

    fix search domain

--- Bash_Loop/two_var.sh
+++ Bash_Loop/two_var.sh
@@ -5,7 +5,7 @@ do for n_eval in  3000 5000
 do for lb in {-3..1}
 do for ub in {1..4}
 do
-cat <<EnD>"jobs/$p-2-$lb-$ub-$n_eval-$s".sh
+cat <<EnD>"jobs/$p-2-[$lb-$ub]-$s-$n_eval".sh
 #!/bin/bash
 #PBS -N job"$i"
 #PBS -l ncpus=1,mem=20GB
@@ -21,10 +21,10 @@ cat <<EnD>"jobs/$p-2-$lb-$ub-$n_eval-$s".sh
 #module load gcc/11.1.0
 #module load intel-mkl/2020.3.304  python3/3.9.2
 
-echo python3 NSGA-II.py -p $p -s $s -n_eval $n_eval -lb $lb $lb -ub  $ub $ub -d 2 -f "_$p_$d_$lb_$ub_$n_eval_$s" 
+echo python3 NSGA-II.py -p $p -s $s -n_eval $n_eval -lb $lb $lb -ub  $ub $ub -d 2 -f "$p_2_$lb_$ub_$n_eval_$s" 
 EnD
 
-qsub "jobs/$p-2-$lb-$ub-$n_eval-$s".sh
+qsub "/home/582/ow6835/MOOP/Bash_Loop/jobs/$p-2-[$lb-$ub]-$s-$n_eval".sh
 echo $PBS_JOBID
 sleep 1
 done
@@ -32,3 +32,5 @@ done
 done
 done 
 done
+
+
