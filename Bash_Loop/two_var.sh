26d011cdc68cf8364e893adef96effe2241368aa
Oscarwu <wuyuta@deakin.edu.au>
2021-08-23 17:38:40 +1000

    updated writted path in bash

--- Bash_Loop/two_var.sh
+++ Bash_Loop/two_var.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-for p in $(cat Bash_Loop/problem_txt/two_var_problems.txt)
+for p in $(cat /home/582/ow6835/MOOP/Bash_Loop/problem_txt/two_var_problems.txt)
 do for s in 50 100 150
 do for n_eval in  3000 5000 
 do for lb in {-3..1}
