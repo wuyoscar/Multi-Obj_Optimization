782ac99c024b0604f9fab6780c579b728d128fec
Oscarwu <wuyuta@deakin.edu.au>
2021-08-23 23:13:30 +1000

    path updated

--- Bash_Loop/two_var.sh
+++ Bash_Loop/two_var.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-for p in $(cat /home/582/ow6835/MOOP/Bash_Loop/problem_txt/two_var_problems.txt)
+for p in $(cat /scratch/lk32/ow6835/MOOP/Bash_Loop/problem_txt/two_var_problems.txt)
 do for s in 50 100 150
 do for n_eval in  3000 5000 
-do for lb in {-3..1}
+do for lb in {-3..-2}
 do for ub in {1..4}
 do
 cat <<EnD>/scratch/lk32/ow6835/MOOP/Bash_Loop/jobs/$p-2-[$lb-$ub]-$s-$n_eval.sh
@@ -12,6 +12,7 @@ cat <<EnD>/scratch/lk32/ow6835/MOOP/Bash_Loop/jobs/$p-2-[$lb-$ub]-$s-$n_eval.sh
 #PBS -l walltime=24:00:00
 #PBS -P lk32
 #PBS -q normal
+#PBS -l storage=/scratch/lk32/ow6835
 #PBS -l wd
 #PBS -o /scratch/lk32/ow6835/JOBOUTS
 #PBS -e /scratch/lk32/ow6835/JOBOUTS
@@ -23,7 +24,7 @@ cat <<EnD>/scratch/lk32/ow6835/MOOP/Bash_Loop/jobs/$p-2-[$lb-$ub]-$s-$n_eval.sh
 #module load gcc/11.1.0
 #module load intel-mkl/2020.3.304  python3/3.9.2
 
-python3 NSGA-II.py -p $p -s $s -n_eval $n_eval -lb $lb $lb -ub  $ub $ub -d 2 -f "$p_2_$lb_$ub_$n_eval_$s">$PBS_JOBID.log
+python3 /scratch/lk32/ow6835/MOOP/NSGA-II.py -p $p -s $s -n_eval $n_eval -lb $lb $lb -ub  $ub $ub -d 2 -f $p_2_$lb_$ub_$n_eval_$s>$PBS_JOBID.log
 EnD
 
 qsub /scratch/lk32/ow6835/MOOP/Bash_Loop/jobs/$p-2-[$lb-$ub]-$s-$n_eval.sh
