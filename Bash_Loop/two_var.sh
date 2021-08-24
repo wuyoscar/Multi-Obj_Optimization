268098ded6ad519c238cd0c6de00130be6094583
Oscarwu <wuyuta@deakin.edu.au>
2021-08-24 14:19:07 +1000

    change parameters

--- Bash_Loop/two_var.sh
+++ Bash_Loop/two_var.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 for p in $(cat /scratch/lk32/ow6835/MOOP/Bash_Loop/problem_txt/two_var_problems.txt)
-do for s in 50 100 150
-do for n_eval in  3000 5000 
-do for lb in {-3..-2}
+do for s in 50 100 150 200
+do for n_eval in  3000 5000 7000 8000
+do for lb in {-3..-1}
 do for ub in {1..4}
 do
 cat <<EnD>/scratch/lk32/ow6835/MOOP/Bash_Loop/jobs/$p-2-[$lb,$ub]-$s-$n_eval.sh
@@ -22,7 +22,7 @@ cat <<EnD>/scratch/lk32/ow6835/MOOP/Bash_Loop/jobs/$p-2-[$lb,$ub]-$s-$n_eval.sh
 module load gcc/11.1.0
 module load intel-mkl/2020.3.304  python3/3.9.2
 
-python3 /scratch/lk32/ow6835/MOOP/NSGA-II.py -p $p -s $s -n_eval $n_eval -lb $lb $lb -ub  $ub $ub -d 2 -f $p-2-[$lb,$ub]-$n_eval-$s>$PBS_JOBID.log
+python3 /scratch/lk32/ow6835/MOOP/NSGA-II.py -p $p -s $s -n_eval $n_eval -lb $lb $lb -ub  $ub $ub -d 2 -f $p-2-[$lb,$ub]-$n_eval-$s
 EnD
 
 qsub /scratch/lk32/ow6835/MOOP/Bash_Loop/jobs/$p-2-[$lb-$ub]-$s-$n_eval.sh
