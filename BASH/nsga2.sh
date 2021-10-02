f775177f69ed6d101060d2f6dee8a3a08136261c
Oscarwu <wuyuta@deakin.edu.au>
2021-10-02 14:56:50 +1000

    change path

--- BASH/nsga2.sh
+++ BASH/nsga2.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+mkdir bash_jobs
 while read -ra p
 do for s in 500 1000
 do for n_eval in 3000 5000 8000
@@ -7,7 +8,7 @@ do for ub in {1..3}
 do
 # problem, problem dimension, #objectives, data size, lower bound, upper bound, #evaluations
 # create PBS job with paramaters 
-cat <<EnD>/scratch/lk32/ow6835/MOOP_Result/bash_jobs/${p[0]}_${p[1]}_${p[2]}_nsga2.job_${s}_${lb}_$(($lb+$ub))_${n_eval} 
+cat <<EnD>bash_jobs/${p[0]}_${p[1]}_${p[2]}_nsga2.job_${s}_${lb}_$(($lb+$ub))_${n_eval} 
 #!/bin/bash
 #PBS -l ncpus=1,mem=2GB
 #PBS -l walltime=24:00:00
@@ -28,7 +29,7 @@ EnD
 
 #submited jobs
 echo -p ${p[0]} -s $s -n_eval $n_eval -lb $lb  -ub  $(($lb+$ub)) -d ${p[1]} -o ${p[2]} -f ${p[0]}_${p[1]}_${p[2]}_nsga2.job_${s}_${lb}_$(($lb+$ub))_${n_eval}
-qsub /scratch/lk32/ow6835/MOOP_Result/bash_jobs/${p[0]}_${p[1]}_${p[2]}_nsga2.job_${s}_${lb}_$(($lb+$ub))_${n_eval}
+qsub bash_jobs/${p[0]}_${p[1]}_${p[2]}_nsga2.job_${s}_${lb}_$(($lb+$ub))_${n_eval}
 
 
 done
