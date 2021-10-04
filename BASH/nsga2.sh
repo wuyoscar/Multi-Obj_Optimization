66dbe2f60e9edb7b75a49440d90c83d2402f3ad3
Oscarwu <wuyuta@deakin.edu.au>
2021-10-04 21:57:56 +1100

    updated

--- BASH/nsga2.sh
+++ BASH/nsga2.sh
@@ -8,7 +8,7 @@ do for ub in {1..3}
 do
 # problem, problem dimension, #objectives, data size, lower bound, upper bound, #evaluations
 # create PBS job with paramaters 
-cat <<EnD>bash_jobs/${p[0]}_${p[1]}_${p[2]}_nsga2.job_${s}_${lb}_$(($lb+$ub))_${n_eval} 
+cat <<EnD>bash_jobs/${p[0]}_${p[1]}_${p[2]}_nsga2.job_${s}_${lb}_${ub}_${n_eval} 
 #!/bin/bash
 #PBS -l ncpus=1,mem=2GB
 #PBS -l walltime=24:00:00
@@ -16,27 +16,28 @@ cat <<EnD>bash_jobs/${p[0]}_${p[1]}_${p[2]}_nsga2.job_${s}_${lb}_$(($lb+$ub))_${
 #PBS -q normal
 #PBS -l wd
 #PBS -m n
-#PBS -o /scratch/lk32/ow6835/MOOP_Result/bash_jobs_output
-#PBS -e /scratch/lk32/ow6835/MOOP_Result/bash_jobs_error_output
+#PBS -o /scratch/lk32/ow6835/MOOP_Result/bash_jobs_output/
+#PBS -e /scratch/lk32/ow6835/MOOP_Result/bash_jobs_error_output/
 
 
 module load gcc/11.1.0
 module load intel-mkl/2020.3.304  python3/3.9.2
 
-python3 /scratch/lk32/ow6835/MOOP/NSGA-II.py -p ${p[0]} -s $s -n_eval $n_eval -lb $lb  -ub  $(($lb+$ub)) -d ${p[1]} -o ${p[2]} -f ${p[0]}_${p[1]}_${p[2]}_nsga2.job_${s}_${lb}_$(($lb+$ub))_${n_eval}
+python3 /scratch/lk32/ow6835/MOOP/NSGA-II.py -p ${p[0]} -s $s -n_eval $n_eval -lb $lb  -ub  ${ub} -d ${p[1]} -o ${p[2]} -f ${p[0]}_${p[1]}_${p[2]}_nsga2.job_${s}_${lb}_${ub}_${n_eval}
 
 EnD
 
 #submited jobs
-echo -p ${p[0]} -s $s -n_eval $n_eval -lb $lb  -ub  $(($lb+$ub)) -d ${p[1]} -o ${p[2]} -f ${p[0]}_${p[1]}_${p[2]}_nsga2.job_${s}_${lb}_$(($lb+$ub))_${n_eval}
-qsub bash_jobs/${p[0]}_${p[1]}_${p[2]}_nsga2.job_${s}_${lb}_$(($lb+$ub))_${n_eval}
+#echo -p ${p[0]} -s $s -n_eval $n_eval -lb $lb  -ub  ${ub} -d ${p[1]} -o ${p[2]} -f ${p[0]}_${p[1]}_${p[2]}_nsga2.job_${s}_${lb}_${ub}_${n_eval}
+qsub bash_jobs/${p[0]}_${p[1]}_${p[2]}_nsga2.job_${s}_${lb}_${ub}_${n_eval}
 
+/scratch/lk32/ow6835/paretofront /scratch/lk32/ow6835/MOOP_Result/Result/output_X/${p[0]}_${p[1]}_${p[2]}_nsga2.job_${s}_${lb}_${ub}_${n_eval} /scratch/lk32/ow6835/MOOP_Result/paratofront/pf_${p[0]}_${p[1]}_${p[2]}_nsga2.job_${s}_${lb}_${ub}_${n_eval}  ${p[1]}  ${s} 1 0 
 
 done
 done
 done
 done 
-done </scratch/lk32/ow6835//MOOP/BASH/problems.txt
+done </scratch/lk32/ow6835/MOOP/BASH/problems.txt
 
 
 
