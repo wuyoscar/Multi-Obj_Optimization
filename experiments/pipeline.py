06092bcb2b6e824ce237d7c32f0ea8504a0b8605
Oscar WU <ow6835@gadi-login-06.gadi.nci.org.au>
2021-11-01 17:41:40 +1100

    result update

--- experiments/pipeline.py
+++ experiments/pipeline.py
@@ -147,7 +147,7 @@ if __name__ == "__main__":
             'image_location':image_location
     }   
 
-    table_path = os.path.join(currentdir,'Result','Kursawe_Result')
+    table_path = os.path.join(currentdir,'Result','Zdt_Result')
     file_exists = os.path.isfile(table_path)
     with open(table_path, 'a+', encoding='UTF8', newline='') as f:
         writer = csv.DictWriter(f, fieldnames=fieldnames)
