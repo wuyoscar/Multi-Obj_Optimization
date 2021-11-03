3d22ada3aa0fb1b42507e15b4e49ffe855af794b
Oscarwu <wuyuta@deakin.edu.au>
2021-11-03 23:01:00 +1100

    ready to test

--- experiments/pipeline_bash_fixed.sh
+++ experiments/pipeline_bash_fixed.sh
@@ -2,12 +2,12 @@
 i=0
 while read -ra p
 do for a in nsga2 nsga3 agnomen rnsga2
-do for n_gen in 100 200 500 1000 10000 100000  
+do for n_gen in 100 200 500 1000 10000   
 do for pop in 100 200 300 500
 do
 cat <<EnD>/home/582/ow6835/bash_scripts/${p[0]}_${a}_${n_gen}
 #!/bin/bash
-#PBS -l ncpus=4,mem=64GB
+#PBS -l ncpus=4,mem=32GB
 #PBS -l walltime=24:00:00
 #PBS -P lk32
 #PBS -q normal
