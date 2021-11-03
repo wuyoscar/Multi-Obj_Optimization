0815659cb5d0037fc7a94990632a283c7d8f41c7
Oscar WU <ow6835@gadi-login-09.gadi.nci.org.au>
2021-11-03 18:22:23 +1100

    add result here

--- experiments/pipeline.py
+++ experiments/pipeline.py
@@ -150,7 +150,7 @@ if __name__ == "__main__":
             'image_location':image_location
     }   
 
-    table_path = os.path.join(currentdir,'Result','result_with_popsize')
+    table_path = os.path.join(currentdir,'Result','result_with_popsize_large_memory')
     file_exists = os.path.isfile(table_path)
     with open(table_path, 'a+', encoding='UTF8', newline='') as f:
         writer = csv.DictWriter(f, fieldnames=fieldnames)
