ad205c02ed9403d55a184babd72fa7bb69704238
Oscarwu <wuyuta@deakin.edu.au>
2021-11-23 23:18:01 +1100

    ready to larger result

--- experiments/pipeline_large.sh
+++ experiments/pipeline_large.sh
@@ -2,7 +2,7 @@
 i=1
 while read -ra p
 do for a in nsga2 nsga3 agnomen rnsga2
-do for e_val in  50000000  
+do for e_val in  10000000 50000000  
 do for pop in 100  
 do
 cat <<EnD>/home/582/ow6835/bash_scripts/${p[0]}_${a}_${e_val}
